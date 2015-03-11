f0840630b845ee3da78789c77c0f87114393f42b
n1t1nv3rma <nitinsheenu@yahoo.com>
2015-03-11 15:49:52 +1100

    new release

--- docker/recipes/docker-deploy.rb
+++ docker/recipes/docker-deploy.rb
@@ -3,10 +3,10 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   
-  if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
-    Chef::Log.debug("Skipping deploy::docker application #{application} as it is not deployed to this layer")
-    next
-  end
+#  if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
+#    Chef::Log.debug("Skipping deploy::docker application #{application} as it is not deployed to this layer")
+#    next
+#  end
 
   opsworks_deploy_dir do
     user deploy[:user]
