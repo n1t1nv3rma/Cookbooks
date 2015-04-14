b519274788947753d5fa9aa28e62cc749e965192
n1t1nv3rma <nitinsheenu@yahoo.com>
2015-04-14 10:17:53 -0700

    added R53 CB

--- updateR53/metadata.rb
+++ updateR53/metadata.rb
@@ -1,8 +1,9 @@
 name             'updateR53'
 maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'me@example.com'
 license          'all_rights'
 description      'Installs/Configures updateR53'
 long_description 'Installs/Configures updateR53'
 version          '0.1.0'
 
+depends "route53"
