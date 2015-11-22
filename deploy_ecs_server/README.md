4a5e65307397f85e01b0f19019520be64c238a2e
Nitin Verma <nitinsheenu@gmail.com>
2015-11-23 00:02:30 +1100

    updated recipe

--- deploy_ecs_server/README.md
+++ deploy_ecs_server/README.md
@@ -1,4 +1,40 @@
 # deploy_ecs_server
+# Developed by: vernitin@ 
+# User at your own risk.
+# 
+# Use this recipe to deploy an AWS OpsWorks App into your AWS ECS Cluster
 
-TODO: Enter the cookbook description here.
+# Your OpsWorks App must be in folowing format:
+
+# Create a directory called "services" with any number of files listing ECS Services
+
+Ex: OpsWorks App directory structure will be:
+ App:
+  - services
+    - my-ecs-service-1
+    - my-ecs-service-2
+    - my-ecs-service-3
+     ...
+
+# Create any number of files listing ECS Service under "services" folder.
+# For Example: The content of the "services/my-ecs-service-1" file content must be in following standard format i.e. compatible with AWS CLI
+
+---------
+{
+              "cluster": "my-ow-ecs-cluster",
+              "serviceName": "ecs-sample-service-elb",
+              "taskDefinition": "my-sample-console:4",
+              "loadBalancers": [
+                  {
+                      "loadBalancerName": "my-ow-ecs-elb",
+                      "containerName": "simple-app",
+                      "containerPort": 80
+                  }
+              ],
+              "desiredCount": 2,
+              "role": "ecsServiceRole"
+}
+---------
+
+# Deploy this App via OpsWorks on the instances running under ECS Cluster Layer!
 
--- deploy_ecs_server/recipes/default.rb
+++ deploy_ecs_server/recipes/default.rb
@@ -24,15 +24,94 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
- bash "Deploying ECS Services in #{deploy[:deploy_to]}/current/services on #{node[:opsworks][:instance][:hostname]}" do
-  region = #{node[:opsworks][:instance][:region]}
+ bash "Deploying ECS Services in the #{deploy[:deploy_to]}/current/services via #{node[:opsworks][:instance][:hostname]}" do
+  region = "#{node[:opsworks][:instance][:region]}"
   cwd = "#{deploy[:deploy_to]}/current/services"
+  logfile = "/var/tmp/ow-ecs-service-deploy.log"
   user "root"
   code <<-EOH
-  sleep 10
-  cd "#{cwd}" && for SER in `ls`; do echo "Deploying Service ${SER}..."; CONT=`grep containerName ${SER} | awk -F'"' '{print $4}'` ; if ( `docker ps | grep -v grep | grep ${CONT} >/dev/null 2>&1` ) ; then echo "Container: ${CONT} is running currently, deploying an update to the service..."; aws ecs update-service --cli-input-json file://${SER} --region="#{region}"; else echo "Container: ${CONT} is not running currently, deploying new service..."; aws ecs create-service --cli-input-json file://${SER} --region="#{region}"; fi; touch /var/tmp/test-ow-ecs-run; done
-  EOH
-  only_if { ::File.exist?("/usr/bin/docker") && !OpsWorks::ShellOut.shellout("docker ps -a").include?("amazon-ecs-agent") }
+set -x
+update_Task_Def() {
+  echo "`date`: Updating Task Def now..."
+  aws ecs update-service --cluster "${CLUST}" --service "${SVC}" --task-definition "${TDEF}" --region="#{region}"
+}
+update_Count_toDesired() {
+  echo "`date`: Updating Desired Count now..."
+  aws ecs update-service --cluster "${CLUST}" --service "${SVC}" --desired-count ${DCOUNT} --region="#{region}"
+}
+update_Task_Def_and_Count() {
+  echo "`date`: Updating both Task Def and Desired Count now..."
+  aws ecs update-service --cluster "${CLUST}" --service "${SVC}" --task-definition "${TDEF}"  --desired-count ${DCOUNT} --region="#{region}"
+}
+temp_reduce_Count() {
+  echo "`date`: Temporarily reducing the Count now..."
+  TCOUNT=`expr $TASKS - 1`
+  aws ecs update-service --cluster "${CLUST}" --service "${SVC}" --desired-count ${TCOUNT} --region="#{region}"
+}
+wait_for_steady() {
+ until `aws ecs describe-services --cluster "${CLUST}" --service "${SVC}" --region="#{region}" | grep message | head -1 | grep "reached a steady state" >/dev/null 2>&1`
+ do
+   echo "`date`: Waiting for service to reach a steady state after deploying new service..."; sleep 5;
+ done
+}
+# Main goes here
+ cd "#{cwd}" 
+ for SER_FILE in `ls`; 
+ do 
+  echo "Deploying Service ${SER_FILE}...";
+  SVC=`grep serviceName ${SER_FILE} | awk -F'"' '{print $4}'` ; 
+  CLUST=`grep cluster ${SER_FILE} | awk -F'"' '{print $4}'`;
+  DCOUNT=`grep desiredCount ${SER_FILE} | awk -F':' '{print $2}' | sed -e 's/,//'`;
+  TDEF=`grep taskDefinition ${SER_FILE} | awk -F'"' '{print $4}'`;
+  LIST_SVC=`aws ecs list-services --cluster ${CLUST} --region="#{region}" | cut -d'/' -f2 | sed -e 's/"//' -e 's/,//' | grep -x "${SVC}"`
+  # Check if service exist
+  if [ "${LIST_SVC}" != "" ]
+    then 
+      echo "`date`: Service: ${SVC} already existing, updating it..."; 
+      echo "`date`: Current issues: check for the need to prepare for rolling update!"
+      TASKS=`aws ecs list-tasks --cluster  ${CLUST} --service ${SVC} --region="#{region}" | grep arn | wc -l`
+      TOTAL_TASKS=`expr $DCOUNT + $TASKS`
+      INSTANCE=`aws ecs list-container-instances --cluster ${CLUST} --region="#{region}" | grep arn | wc -l`
+
+      if [ $TOTAL_TASKS -le $INSTANCE ]
+        then
+          echo "`date`: No need to prepare, updating the Service ${SVC} now..."
+          update_Task_Def_and_Count;
+        else
+          echo "`date`: Preparing for rolling update of the Service ${SVC} now..."
+          if [ $TASKS -lt $DCOUNT ]
+            then
+              echo "Tasks are less than Desired count..."
+              update_Task_Def;
+              echo "`date`: Waiting for service to reach a steady state"
+              sleep 60;
+              wait_for_steady;
+              update_Count_toDesired;
+            elif [ $TASKS -eq $DCOUNT ]
+            then
+              echo "Tasks are equal to Desired count..."
+              temp_reduce_Count;
+              update_Task_Def;
+              echo "`date`: Waiting for service to reach a steady state"
+              sleep 60;
+              wait_for_steady;
+              update_Count_toDesired;
+            else
+              echo "Tasks are more than Desired count..."
+              update_Count_toDesired;
+              echo "`date`: Waiting for service to reach a steady state"
+              sleep 60;
+              wait_for_steady;
+              update_Task_Def;
+          fi
+      fi
+    else
+     echo "`date`: Service: ${SVC} is not running currently, deploying new service..."
+     aws ecs create-service --cli-input-json file://${SER_FILE} --region="#{region}"
+  fi
+  done >> "#{logfile}" 2>&1
+EOH
+  only_if { ::File.exist?("/usr/bin/aws") }
  end
 
 end
