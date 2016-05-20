e876b5751ae633c65e6bcabdb144ac7d9a48d706
Nitin Verma <nitinsheenu@gmail.com>
2016-05-20 13:11:02 +1000

    rename packages

---
+++ mypackages/recipes/default.rb
@@ -0,0 +1,14 @@
+yum_package 'mysql-connector-java' do
+  action :remove
+end
+
+remote_file "/var/tmp/mysql-connector-java-5.1.36-1.fc23.noarch.rpm" do
+  source "https://s3-ap-northeast-1.amazonaws.com/mytestbucket-nv/mysql-connector-java-5.1.36-1.fc23.noarch.rpm";
+  notifies :install, "yum_package[mysql-connector-java-new]", :immediately
+end
+
+yum_package 'mysql-connector-java-new' do
+  source "/var/tmp/mysql-connector-java-5.1.36-1.fc23.noarch.rpm"
+  not_if "rpm -qa | grep -q '^mysql-connector-java'"
+  action :install
+end
